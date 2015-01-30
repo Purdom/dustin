489cf6c31c40ba4911a1b043b984984dacc3e0e6
Purdom <lindbla1@gmail.com>
2015-01-30 12:01:05 -0500

    adds project navigation menu on homepage

--- dustin.js
+++ dustin.js
@@ -2,5 +2,34 @@ jQuery(document).ready(function($) {
   //alert('hooray');
 
  $('#socialmedia-container').clone().appendTo('#header .inside');
-});
 
+ var projectNav = '<ul id="filterNav" class="clearfix">'
+                + '<li class="segment-0"><a href="#" data-filter="*" class="selected">All</a></li>'
+                + '<li><a href="#" data-filter=".branding">Branding</a></li>'
+                + '<li><a href="#" data-filter=".commercial">Commercial</a></li>'
+                + '<li><a href="#" data-filter=".film-projects">Film</a></li>'
+                + '<li><a href="#" data-filter=".illustration">Illustration</a></li>'
+                +  '</ul>';
+
+$(projectNav).prependTo('#homeProjects');
+
+	jQuery('#filterNav a').click(function(){
+		var projectType = jQuery(this).attr('data-filter');	
+    alert(projectType);
+		jQuery('#homeProjects .thumbs').isotope({
+			filter: projectType,
+      hiddenStyle : {
+			    opacity: 0,
+			    scale : 1
+			}
+		});
+	
+		if ( !jQuery(this).hasClass('selected') ) {
+			jQuery(this).parents('#filterNav').find('.selected').removeClass('selected');
+			jQuery(this).addClass('selected');
+		}
+		
+		return false;
+	});
+
+});
